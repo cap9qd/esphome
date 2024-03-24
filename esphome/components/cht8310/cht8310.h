948e3cdfcae59e8fa5516b18307b86baa554369c
cap9qd <33727568+cap9qd@users.noreply.github.com>
2024-03-24 14:13:12 -0500

    Changes to add module CHT8310 and add warning to incompatible hdc1080.
    Chip CHT8305 is compatible with HDC1080 but the CHT8310 has different registers and temperature/humidity conversions. Add module for this instead of adding to the HDC1080.

--- esphome/components/cht8310/cht8310.h
+++ esphome/components/cht8310/cht8310.h
@@ -5,13 +5,20 @@
 #include "esphome/components/i2c/i2c.h"
 
 namespace esphome {
-namespace hdc1080 {
+namespace cht8310 {
 
-class HDC1080Component : public PollingComponent, public i2c::I2CDevice {
+class CHT8310Component : public PollingComponent, public i2c::I2CDevice {
  public:
   void set_temperature(sensor::Sensor *temperature) { temperature_ = temperature; }
   void set_humidity(sensor::Sensor *humidity) { humidity_ = humidity; }
-
+  
+  /*
+  void set_max_temperature(float max_temperature) { max_temperature_ = max_temperature; }
+  void set_min_temperature(float min_temperature) { min_temperature_ = min_temperature; }
+  void set_max_humidity(float max_humidity) { max_humidity_ = max_humidity; }
+  void set_min_humidity(float min_humidity) { min_humidity_ = min_humidity; }
+  */
+  
   /// Setup the sensor and check for connection.
   void setup() override;
   void dump_config() override;
@@ -23,9 +30,14 @@ class HDC1080Component : public PollingComponent, public i2c::I2CDevice {
  protected:
   sensor::Sensor *temperature_{nullptr};
   sensor::Sensor *humidity_{nullptr};
-  
+  /*
+  optional<float> min_temperature_;
+  optional<float> max_temperature_;
+  optional<float> min_humidity_;
+  optional<float> max_humidity_;
+  */
   uint16_t chip_ver;
 };
 
-}  // namespace hdc1080
+}  // namespace cht8310
 }  // namespace esphome
