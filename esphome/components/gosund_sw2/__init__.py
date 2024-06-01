f4fdd854fcc3b9f48c87e998afb4b7b50c1a5a3a
cap9qd <33727568+cap9qd@users.noreply.github.com>
2024-06-01 09:18:53 -0500

    Add GoSund SW2 device component

---
+++ esphome/components/gosund_sw2/__init__.py
@@ -0,0 +1,53 @@
+import esphome.codegen as cg
+from esphome.components import light, output, uart
+import esphome.config_validation as cv
+from esphome.const import (
+    CONF_OUTPUT_ID,
+    CONF_GAMMA_CORRECT,
+    CONF_DEFAULT_TRANSITION_LENGTH,
+    PLATFORM_ESP8266,
+    PLATFORM_BK72XX,
+)
+
+DEPENDENCIES = ["uart"]
+
+gosund_sw2_ns = cg.esphome_ns.namespace("gosund")
+GoSundSw2Component = gosund_sw2_ns.class_(
+    "GosundLight", light.LightOutput, cg.Component
+)
+
+CONF_MCU_VER = "mcu_ver"
+CONF_STATUS_OUTPUT = "status_output"
+CONF_DEBUG = "debug"
+
+CONFIG_SCHEMA = cv.All(
+    light.BRIGHTNESS_ONLY_LIGHT_SCHEMA.extend(
+        {
+            cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(GoSundSw2Component),
+            cv.Required(CONF_MCU_VER): cv.int_,
+            cv.Required(CONF_STATUS_OUTPUT): cv.use_id(output.BinaryOutput),
+            cv.Optional(CONF_GAMMA_CORRECT, default=1.0): cv.positive_float,
+            cv.Optional(
+                CONF_DEFAULT_TRANSITION_LENGTH, default="0s"
+            ): cv.positive_time_period_milliseconds,
+            cv.Optional(CONF_DEBUG): cv.boolean,
+        }
+    )
+    .extend(cv.COMPONENT_SCHEMA)
+    .extend(uart.UART_DEVICE_SCHEMA),
+    cv.only_on([PLATFORM_ESP8266, PLATFORM_BK72XX]),
+)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
+    await cg.register_component(var, config)
+    await light.register_light(var, config)
+    await uart.register_uart_device(var, config)
+
+    cg.add(var.set_mcu_version(config[CONF_MCU_VER]))
+    if CONF_DEBUG in config:
+        cg.add(var.set_debug(config[CONF_DEBUG]))
+
+    light_ = await cg.get_variable(config[CONF_STATUS_OUTPUT])
+    cg.add(var.set_light(light_))
---
+++ esphome/components/gosund_sw2/gosund_sw2_light.hpp
@@ -0,0 +1,46 @@
+#pragma once
+#include "esphome.h"
+#include "esphome/components/uart/uart.h"
+#include "esphome/components/light/light_output.h"
+
+namespace esphome
+{
+    namespace gosund
+    {
+        class GosundLight : public Component, public light::LightOutput, public uart::UARTDevice{
+
+            public:
+                void setup() override;
+                void loop() override;
+                
+                light::LightTraits get_traits() override;
+                
+                void setup_state(light::LightState *state) override { state_ = state; }
+                void write_state(light::LightState *state) override;
+                
+                void dump_config();
+                void set_debug(bool debug);
+                void set_mcu_version(int mcu_ver);
+                void set_light(output::BinaryOutput *light) { this->status_led_ = light; }
+                //void set_uart(uart::UARTDevice *uart) {this->uart_ = uart;}
+                
+            protected:
+                light::LightState *state_{nullptr};
+                output::BinaryOutput *status_led_;
+                //uart::UARTDevice *uart_;
+                uint8_t mcuVer = 0;
+                bool debugPrint = false;
+                
+                const char *TAG = "gosund.light.sw2";
+                const byte ON_MASK = 0x80;
+
+                const uint8_t MAX_PERCENT = 100;
+                const uint8_t MIN_PERCENT = 1;
+
+                float MAX_VALUE = 100.0;
+
+                bool setupError = 0;
+        };
+
+    } // namespace gosund
+} // namespace esphome
