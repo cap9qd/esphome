3c1ebbfc863f614e7509e140d667c612c969c191
cap9qd <33727568+cap9qd@users.noreply.github.com>
2025-08-29 19:55:42 -0500

    Update __init__.py

--- esphome/components/deep_sleep/__init__.py
+++ esphome/components/deep_sleep/__init__.py
@@ -253,7 +253,6 @@ WAKEUP_CAUSES_SCHEMA_LT = cv.Schema(
 
 WAKEUP_PIN_CONF_SCHEMA_LT = cv.Schema(
     {
-#        cv.Required(CONF_NUMBER): cv.int_,
         cv.Required(CONF_PIN): pins.internal_gpio_input_pin_schema,
         cv.Required(CONF_PIN_MODE): cv.enum(LT_WAKEUP_PIN_MODES, upper=True),
     }
@@ -304,7 +303,7 @@ CONFIG_SCHEMA = cv.All(
             ),
             cv.Optional(CONF_BK71XX_GPIO_WAKEUP): cv.All(
                 cv.only_on(PLATFORM_BK72XX),
-                cv.ensure_list(WAKEUP_PIN_CONF_SCHEMA_LT),
+                cv.Schema({cv.ensure_list(WAKEUP_PIN_CONF_SCHEMA_LT)}),
 #                validate_pin_number_lt,
             ),
         }
