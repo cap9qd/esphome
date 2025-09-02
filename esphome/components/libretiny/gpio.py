aee9ff9112e0a820de268a4ddb2156a86b664e45
cap9qd <33727568+cap9qd@users.noreply.github.com>
2025-09-01 20:13:27 -0500

    Update gpio.py

--- esphome/components/libretiny/gpio.py
+++ esphome/components/libretiny/gpio.py
@@ -176,6 +176,8 @@ def validate_gpio_usage(value):
         (True, False, False, False, True),
         # OUTPUT_OPEN_DRAIN
         (False, True, True, False, False),
+        # INPUT_OUTPUT_I2C
+        (True, True, False, False, False),
     }
     key = (is_input, is_output, is_open_drain, is_pullup, is_pulldown)
     if key not in supported_modes:
